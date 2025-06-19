4cbc6b7bc7b16e3db7eb55e2400867b5c3090956
Nipunika Singh <nipunikasingh.2017@gmail.com>
2025-06-19 20:18:59 +0530

    namaste

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It demon basic output functionality.");
+        System.out.println("nn");
+    }
+}
+
+
